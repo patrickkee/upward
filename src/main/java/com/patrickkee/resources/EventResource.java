8b07df5c34ab337b5e3208f6d7d7d58d7f9aeaf1
Patrick Kee <patrick.kee0@gmail.com>
2016-02-13 23:56:08 -0500

    added update event resource and integration test for it

--- src/main/java/com/patrickkee/resources/EventResource.java
+++ src/main/java/com/patrickkee/resources/EventResource.java
@@ -25,6 +25,14 @@ import com.patrickkee.persistence.FinancialModelsDb;
 @Path("accounts/{email}/models/{modelId}/events")
 public class EventResource {
 
+	/**
+	 * Creates a new event in a given model 
+	 * @param email
+	 * @param modelId
+	 * @param event
+	 * @param uriInfo
+	 * @return
+	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -46,6 +54,33 @@ public class EventResource {
 			return Response.created(locationBuilder.build()).entity(ResponseMessage.getNew("EVENT_CREATION_SUCCESSFUL",
 					"The event was successfully created and persisted to the model")).build();
 
+		} else {
+			throw new WebApplicationException(404);
+		}
+	}
+	
+	@Path("/{eventId}")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateEvent(@PathParam("email") String email, @PathParam("modelId") int modelId, 
+								@PathParam("eventId") int eventId, Event updatedEvent, @Context UriInfo uriInfo) {
+
+		// Update the event in model only if the account, model, and event are found
+		Optional<Account> acct = FinancialModelsDb.getAccount(email);
+		if (acct.isPresent() && acct.get().getModel(modelId).isPresent() && 
+			acct.get().getModel(modelId).get().getEvent(eventId) != null && updatedEvent != null) {
+			Optional<Model> model = acct.get().getModel(modelId);
+
+			model.get().addOrUpdateEvent(updatedEvent);
+
+			UriBuilder locationBuilder = uriInfo.getAbsolutePathBuilder();
+			locationBuilder.path(Integer.toString(updatedEvent.getEventId()));
+
+			// Persist the account with the new event
+			FinancialModelsDb.persistAccount(acct.get());
+			return Response.created(locationBuilder.build()).entity(ResponseMessage.getNew("EVENT_UPDATE_SUCCESSFUL",
+					"The event was successfully updated and persisted to the model")).build();
 		} else {
 			throw new WebApplicationException(404);
 		}
--- src/test/java/resources/EventResourceTest.java
+++ src/test/java/resources/EventResourceTest.java
@@ -72,6 +72,56 @@ public class EventResourceTest extends BaseJerseyTest {
 	}
 
 	@Test
+	public void updateEventTest() {
+		final String EMAIL = "createEventTest@gmail.com";
+		final String START_DATE = "01/25/2010";
+		final String END_DATE = "01/25/2020";
+		Response response = target("accounts").queryParam("accountName", "createModelTest")
+				.queryParam("firstName", "createModelTestFirstName").queryParam("lastName", "createModelTestLastName")
+				.queryParam("email", EMAIL).request(MediaType.APPLICATION_JSON_TYPE)
+				.post(Entity.entity("foo", MediaType.APPLICATION_JSON_TYPE), Response.class);
+
+		response = target("accounts/" + EMAIL + "/models").queryParam("modelName", "createModelTestModelName")
+				.queryParam("description", "createModelTestModelDesc").queryParam("initialValue", 2000)
+				.queryParam("targetValue", 20000).queryParam("startDate", START_DATE).queryParam("endDate", END_DATE)
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.post(Entity.entity("foo", MediaType.APPLICATION_JSON_TYPE), Response.class);
+
+		SavingsForecastModel aModel = response.readEntity(SavingsForecastModel.class);
+		final String MODEL_ID = Integer.toString(aModel.getModelId());
+
+		
+		//Build the event to be created
+		DateTime dt = DATE_FORMATTER.parseDateTime(START_DATE);
+		LocalDate startDate = new LocalDate(dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth());
+
+		dt = DATE_FORMATTER.parseDateTime(END_DATE);
+		LocalDate endDate = new LocalDate(dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth());
+
+		RecurringDeposit event = RecurringDeposit.getNew("Payroll Contribution", Periods.MONTHLY, startDate, endDate, BigDecimal.valueOf(101.24));
+		
+		int eventId = event.getEventId(); //Store for validation of location response
+		
+		//Post the new event
+		response = target("accounts/" + EMAIL + "/models/" + MODEL_ID + "/events")
+					.request(MediaType.APPLICATION_JSON_TYPE)
+					.post(Entity.entity(event, MediaType.APPLICATION_JSON_TYPE), Response.class);
+
+		//Create a new modified event with the same id/name and post it
+		event = RecurringDeposit.getNew("Payroll Contribution", Periods.MONTHLY, startDate, endDate, BigDecimal.valueOf(200));
+		response = target("accounts/" + EMAIL + "/models/" + MODEL_ID + "/events/" + String.valueOf(event.getEventId()))
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.post(Entity.entity(event, MediaType.APPLICATION_JSON_TYPE), Response.class);
+		
+		ResponseMessage respMsg = response.readEntity(ResponseMessage.class);
+		assertEquals(201, response.getStatus());
+		assertTrue(response.getLocation().toString().contains("/accounts/createEventTest@gmail.com/models/" + 
+															   MODEL_ID+"/events/" + Integer.toString(eventId)));
+		assertEquals("EVENT_UPDATE_SUCCESSFUL", respMsg.getMessage());
+		assertEquals("The event was successfully updated and persisted to the model", respMsg.getDescription());
+	}
+	
+	@Test
 	public void getEventsTest() {
 		final String EMAIL = "getEventsTest@gmail.com";
 		final String START_DATE = "01/25/2010";
